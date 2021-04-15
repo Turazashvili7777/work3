dd89f23bcc5ca043267c435af3756ac0e0759f4d
Turazashvili7777 <turazashvili2021@mail.ru>
2021-04-15 19:15:15 +0600

    work

---
+++ lesson-3/script.js
@@ -0,0 +1,66 @@
+// let num = 10
+
+
+// while (num <= 20) {
+//     document.write(`<P>${num * num}</P>`)
+//     num++
+
+// }
+//       (2)
+
+// for (let i = 10; i <= 20; i += 1) {
+//     document.write(`<P>${i * i}</P>`)
+// }
+
+// /////    (2)
+
+
+// let sum = 0
+// for (let i = 54; i <= 234; i += 1) {
+//     sum += i
+// }
+// document.write(`<P>${sum}</P>`)
+
+
+// //    (3)
+
+
+// let num3 = 24
+// let counteven = 0
+// let countodd = 0
+
+// while (num3 <= 98) {
+//     if (num3 % 2 == 0) {
+//         counteven += 1
+//     } else {
+//         countodd += 1
+//     }
+
+//     num3++
+
+// }
+// document.write(`<P>${counteven} : ${countodd}</P>`)
+
+//     (4)
+
+// let countf = 10
+
+// let first = 1
+// let second = 2
+// document.write(` ${first} ${second} `)
+
+// for (let i = 0; i < countf; i++) {
+//     document.write(`${first + second}`)
+//     let temp = first //1
+//     first = second //2
+//     second = temp + first // 1+2+3
+
+// }
+
+// 1 2 3 5 8 13 21 
+
+// (1+2) = 3
+// (2+3) = 5
+// (3+5) = 8
+// (5+8) = 13
+// (8+13)= 21
